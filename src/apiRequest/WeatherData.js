81757eaefea2924f427aedd3e8fe7ba71e3ff22a
IvanRi <riboldiip@globalhitss.com>
2020-06-09 17:18:20 -0300

    add lang es

--- src/apiRequest/WeatherData.js
+++ src/apiRequest/WeatherData.js
@@ -3,6 +3,7 @@ const myApiKey = '6807cadafaf85785dd46ad1417d1e9e7';
 
 // get weather data
 export const getWeatherData = () => {
-  return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${myLocation.lat}&lon=${myLocation.lon}&appid=${myApiKey}&units=metric`)
+  return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${myLocation.lat}&lon=${myLocation.lon}&appid=${myApiKey}&units=metric&lang=es
+  `)
     .then(res => res.json())
 };

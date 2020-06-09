34c79a7ad1c42375858275cea9df4b305ec19ed9
IvanRi <riboldiip@globalhitss.com>
2020-06-08 22:10:04 -0300

    added get weather data endpoint

---
+++ src/apiRequest/WeatherData.js
@@ -0,0 +1,8 @@
+const myLocation = { lat: -34.639239, lon: -58.504896 };
+const myApiKey = '6807cadafaf85785dd46ad1417d1e9e7';
+
+// get weather data
+export const getWeatherData = () => {
+  return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${myLocation.lat}&lon=${myLocation.lon}&appid=${myApiKey}&units=metric`)
+    .then(res => res.json())
+};

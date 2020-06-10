5b13fef97c6e530acbaf4b4c07904a9263af8c12
IvanRi <riboldiip@globalhitss.com>
2020-06-10 13:25:37 -0300

    Updates

---
+++ precache-manifest.3dc854009a2d4afdf9e50b43f277aafe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0b0be7a7ecad78368b870e6a906975",
+    "url": "/InterviewTest/index.html"
+  },
+  {
+    "revision": "ff0ab11b031aca79d241",
+    "url": "/InterviewTest/static/js/2.1b67c167.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/InterviewTest/static/js/2.1b67c167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327679744d9f3de2c41a",
+    "url": "/InterviewTest/static/js/main.203c6a90.chunk.js"
+  },
+  {
+    "revision": "2ba79bf699ff35aa41e8",
+    "url": "/InterviewTest/static/js/runtime-main.1f9f21f7.js"
+  }
+]);
